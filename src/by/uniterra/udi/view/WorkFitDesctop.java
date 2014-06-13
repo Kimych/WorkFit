05a80056fbb30541c0f1bfa63eb05e4edd35e894
Anton K. Nedbailo <anton.nedbailo@uniterra.by>
2014-06-13 15:03:21 +0300

    WorkFit-1
    First real commit

---
+++ src/by/uniterra/udi/view/WorkFitDesctop.java
@@ -0,0 +1,37 @@
+/**
+ * Filename  : WorkFitDesctop.java
+ *
+ * ***************************************************************************
+ * Copyright (C) 2001-2014 by ENVINET GmbH
+ * Hans-Pinsel-Str. 4 
+ * 85540 Haar / Munich
+ * info@envinet.com
+ *                                                      
+ * This material is a CONFIDENTIAL, unpublished work of authorship
+ * created in 2001 and subsequently updated. It is a TRADE SECRET which
+ * is property of ENVINET GmbH.
+ *
+ * All use, disclosure, and/or reproduction not specifically authorized
+ * by ENVINET GmbH in writing is prohibited. All rights reserved.
+ ***************************************************************************
+ * Project    : NMC
+ *
+ * Author     : Anton Nedbailo
+ *
+ * last change by : $Author:$ 
+ * last check in  : $Date: $
+ *
+ */
+
+package by.uniterra.udi.view;
+
+/**
+ * The <code>WorkFitDesctop</code> is used for first test application
+ *
+ * @author Anton Nedbailo
+ * @since Jun 13, 2014
+ */
+public class WorkFitDesctop
+{
+
+}
