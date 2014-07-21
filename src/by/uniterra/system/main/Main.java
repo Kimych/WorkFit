e1e949f2591930a8d682b3a1f3febe558bd9d6b4
SergioAlecky <sergio.alecky@uniterra.by>
2014-07-21 16:32:08 +0300

    #11 create new Main class, contains methods for connect and disconnect to new WorkFit base.

--- src/by/uniterra/system/main/Main.java
+++ src/by/uniterra/system/main/Main.java
@@ -1,4 +1,4 @@
-/*package by.uniterra.system.main;
+package by.uniterra.system.main;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,27 +9,20 @@ import javax.persistence.Persistence;
 
 import org.eclipse.persistence.config.PersistenceUnitProperties;
 
-import by.uniterra.dai.eao.WorkerrService;
-import by.uniterra.dai.entity.OLD_Worker;
 
 public class Main
 {
 
     private static final String PERSISTENCE_UNIT_NAME = "WorkFit";
-    public static int ID_DEL_WORKER = 6;
     
     private static EntityManagerFactory emfFactory;
     private static EntityManager emManager;
     
-
-    *//**,
-     * Extablish a new onnection to target DB
-     *//*
     public static void connectToDB()
     {
         Map<String, String> mapCustomProp = new HashMap<String, String>();
         // put system configuration properties
-        mapCustomProp.put(PersistenceUnitProperties.JDBC_URL, "jdbc:mysql://192.168.1.19:3306/testDB");
+        mapCustomProp.put(PersistenceUnitProperties.JDBC_URL, "jdbc:mysql://192.168.1.19:3306/Workfit");
         mapCustomProp.put(PersistenceUnitProperties.JDBC_USER, "testdb");
         mapCustomProp.put(PersistenceUnitProperties.JDBC_PASSWORD, "testdb");
         mapCustomProp.put(PersistenceUnitProperties.JDBC_DRIVER, "com.mysql.jdbc.Driver");
@@ -40,9 +33,6 @@ public class Main
         emManager = emfFactory.createEntityManager();
     }
     
-    *//**
-     * Disconnect from target DB
-     *//*
     public static void disconnectFromDb()
     {
         // close EntityManager
@@ -56,53 +46,10 @@ public class Main
             emfFactory.close();
         }
     }
-    
 
     public static void main(String[] args)
     {
-
-        // 2.1 Подключиться из приложения к базе данных.
-        connectToDB();
-
-        // 2.2 Прочитать всех Worker и вывезти в консоль все детали (имена,
-        // фамилии, ID, детали связанного Department и т.п.).
-
-        WorkerrService service = new WorkerrService(emManager);
-        service.printAllItems();
-
-        // 2.3 Добавить нового Worker в базу данных.
-        OLD_Worker work1 = new OLD_Worker();
-        work1.setFirstName("worker6");
-        work1.setSekondName("sekond_w6");
-        work1.setThirdName("third_w6");
-        work1.setTelNumber("888888888888");
-        work1.setDepartment(null);
-        service.save(work1);
-
-        // 2.4 Разорвать соединение с базой данных.
-        disconnectFromDb();
-
-        // 2.5 Опять подключиться и вывести данные о всех Worker (убедиться что
-        // наша новая запись в базе).
-        connectToDB();
-        WorkerrService service2 = new WorkerrService(emManager);
-        service2.printAllItems();
-
-        // 2.6 Удалить нашу запись из базы.
-        service2.delete(ID_DEL_WORKER);
-
-        // 2.7 Разорвать соединение с базой данных.
-        disconnectFromDb();
-
-        // 2.8 Опять подключиться и вывести данные о всех Worker (убедиться что
-        // наша новая запись была удалена из базы).
-        connectToDB();
-        WorkerrService service3 = new WorkerrService(emManager);
-        service3.printAllItems();
-
-        // close connection
-        disconnectFromDb();
+	connectToDB();
+	disconnectFromDb();
     }
-
 }
-*/
