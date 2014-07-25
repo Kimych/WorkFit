0e378a99b232cc145a124023d6c0acb22c8d6c3c
SergioAlecky <sergio.alecky@uniterra.by>
2014-07-25 10:59:17 +0300

    #13 transfer junit-4.1

--- src/by/uniterra/test/AllEntitiesTest.java
+++ src/by/uniterra/test/AllEntitiesTest.java
@@ -1,29 +1,28 @@
 package by.uniterra.test;
 
-
-import org.junit.Before;
-import org.junit.Test;
-
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.omg.CORBA.UnknownUserException;
-
-import by.uniterra.dai.entity.Year;
-import by.uniterra.system.main.Task13;
 
 public class AllEntitiesTest
 {
 
-
 	@Before
-	
-
-	@Test(expected = UnknownUserException.class)
-	public void Test1() throws UnknownUserException
+	public void setUp() throws Exception
 	{
+	}
 
-		
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
 	}
 
 }
