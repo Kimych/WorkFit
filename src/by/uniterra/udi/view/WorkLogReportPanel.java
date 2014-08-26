1129fd1b26118f03bdbfb8991d9bcbee44df101e
Anton K. Nedbailo <anton.nedbailo@uniterra.by>
2014-08-26 08:23:29 +0300

    #19 Make "post optimization" for WorkLogReportPanel

--- src/by/uniterra/udi/view/WorkLogReportPanel.java
+++ src/by/uniterra/udi/view/WorkLogReportPanel.java
@@ -15,45 +15,57 @@ import by.uniterra.dai.eao.MonthEAO;
 import by.uniterra.dai.eao.ServiceBaseEAO;
 import by.uniterra.dai.eao.SpentHolidayEAO;
 import by.uniterra.dai.eao.WorkerEAO;
-import by.uniterra.dai.eao.YearEAO;
 import by.uniterra.dai.entity.DaysOfWork;
 import by.uniterra.dai.entity.Worker;
 import by.uniterra.system.util.WorkLogUtils;
 import by.uniterra.udi.iface.IModelOwner;
 import by.uniterra.udi.model.WorkLogInfoHolder;
 
-public class WorkLogReportPanel
+public class WorkLogReportPanel extends JTabbedPane
 {
-    private static List<Worker> workerArrayList;
+    private static final long serialVersionUID = -7127157043829355694L;
 
     public static void main(String[] args)
     {
-        jbInit();
-        ServiceBaseEAO.disconnectFromDb();
+        JFrame frame = new JFrame("Test Employe");
+        frame.setVisible(true);
+        frame.setSize(640, 480);
+        frame.add(new WorkLogReportPanel());
+        frame.setVisible(true);
     }
 
-    public static void jbInit()
+    /**
+     * 
+     * Constructor.
+     *
+     */
+    public WorkLogReportPanel()
     {
-        int curentMonth = YearMonth.now(Clock.systemUTC()).getMonthValue();
-        int numberYear = YearMonth.now(Clock.systemUTC()).getYear();
+	jbInit();
+        ServiceBaseEAO.disconnectFromDb();
+    }
+    
+    /**
+     * Create UI     * 
+     *
+     * @author Anton Nedbailo
+     * @date Aug 26, 2014
+     */
+    private void jbInit()
+    {
+	YearMonth ymCurMonth = YearMonth.now(Clock.systemUTC());
+        int curentMonth = ymCurMonth.getMonthValue();
         
+        // create EAO set
         WorkerEAO eaoWorker = new WorkerEAO(ServiceBaseEAO.getDefaultEM());
-        workerArrayList = eaoWorker.loadAll();
-        JTabbedPane tabbedPane = new JTabbedPane();
         DaysOfWorkEAO eaoDaysOfWork = new DaysOfWorkEAO(ServiceBaseEAO.getDefaultEM());
-
-        // get the number of working days in a month
         MonthEAO eaoMonth = new MonthEAO(ServiceBaseEAO.getDefaultEM());
-        int workingDaysInMonth = eaoMonth.getWorkDayDataForMonth(curentMonth);
-
         SpentHolidayEAO eaoSpentHoliday = new SpentHolidayEAO(ServiceBaseEAO.getDefaultEM());
-        
-
         HolidayEAO eaoHoliday = new HolidayEAO(ServiceBaseEAO.getDefaultEM());
         
-        for (Worker curentWorker : workerArrayList)
+        // for all workers
+        for (Worker curentWorker : eaoWorker.loadAll())
         {
-
             // load DaysOfWork
             List<DaysOfWork> lstDaysOfWork = eaoDaysOfWork.getLastDataForWorkerAndMonthNum(curentWorker, curentMonth);
             Component wlop = null;
@@ -66,15 +78,17 @@ public class WorkLogReportPanel
                 String lastUpdateTime = String.valueOf(lstDaysOfWork.get(0).getTimestamp());
                 // get sum bonus time for the current worker
                 double curentSumBonus = eaoDaysOfWork.getSumBonusTimeForWorkerAndMonthNum(curentWorker, curentMonth);
+                // get the number of working days in a month
+                int workingDaysInMonth = eaoMonth.getWorkDayDataForMonth(curentMonth);
                 // to plane
                 double toPlane = WorkLogUtils.getTimeRemainsToPlane(workingDaysInMonth, workLogTime, curentSumBonus);
                 // get time to bonus
                 double toBonus = WorkLogUtils.getTimeRemainsToBonus(workingDaysInMonth, workLogTime, curentSumBonus);
                 
                 // get  rest of the holiday
+                int numberYear = ymCurMonth.getYear();
                 double holiday = eaoHoliday.getHolidayDaysCountForWorkerAndYear(curentWorker, numberYear);
                 double timeLeft = eaoSpentHoliday.getSpentHolidayWorkerAndYear(curentWorker, numberYear);
-                
                 
                 ((IModelOwner) wlop).setModel(new WorkLogInfoHolder(String.valueOf(workLogTime), toPlane, toBonus, (holiday-timeLeft), lastUpdateTime, curentWorker.toString()));
             }
@@ -82,17 +96,9 @@ public class WorkLogReportPanel
             {
                 wlop = new JLabel("No Data!!! Result lisr size=" + lstDaysOfWork.size());
             }
-
-            tabbedPane.addTab((curentWorker.getFirstName() + " " + curentWorker.getSecondName()), wlop);
-
+            // add a new tab
+            addTab((curentWorker.getFirstName() + " " + curentWorker.getSecondName()), wlop);
         }
-
-        JFrame frame = new JFrame("Test Employe");
-        frame.setVisible(true);
-        frame.setSize(640, 480);
-        frame.add(tabbedPane);
-        frame.setVisible(true);
-
     }
 
 }
