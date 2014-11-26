cca57a331d80d92cd2449468e967e959fa1cb9db
SergioAlecky <sergio.alecky@uniterra.by>
2014-11-26 18:29:23 +0300

    #38 year calendar fixit step

--- src/by/uniterra/udi/view/YearSpecialCalendar.java
+++ src/by/uniterra/udi/view/YearSpecialCalendar.java
@@ -6,13 +6,11 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-import by.uniterra.udi.iface.IModelOwner;
 import by.uniterra.udi.util.Log;
 
 public class YearSpecialCalendar extends JPanel implements ActionListener
@@ -23,11 +21,6 @@ public class YearSpecialCalendar extends JPanel implements ActionListener
 
     HashMap<Integer, MonthSpecialCalendar> mpMonth = new HashMap<Integer, MonthSpecialCalendar>();
 
-    MonthSpecialCalendar msc;
-    MonthSpecialCalendar msc1;
-    MonthSpecialCalendar msc2;
-    MonthSpecialCalendar msc3;
-  
 
     private static final String ACTION_NEXT_YEAR = "Next Year";
     private static final String ACTION_PREVIOUS_YEAR = "Previous Year";
@@ -53,25 +46,11 @@ public class YearSpecialCalendar extends JPanel implements ActionListener
         add(btnPreviousYear, new GridBagConstraints(1, 0, 1, 1, 0, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 5), 0, 0));
         add(btnNextYear, new GridBagConstraints(2, 0, 1, 1, 0, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 5), 0, 0));
         // build calendar 4x3
-        msc = new MonthSpecialCalendar();
-        msc.setModel(0, 2014);
-        mpMonth.put(0, msc);
+  
         
-        msc1 = new MonthSpecialCalendar();
-        msc1.setModel(1, 2014);
-        mpMonth.put(1, msc1);
-        
-        msc2 = new MonthSpecialCalendar();
-        msc2.setModel(2, 2014);
-        mpMonth.put(2, msc2);
-        
-        msc3 = new MonthSpecialCalendar();
-        msc3.setModel(3, 2014);
-        mpMonth.put(3, msc3);
-        
-   /*     for (int i = 0; i < 12; i++)
+        for (int i = 0; i < 12; i++)
         {
-            msc = new MonthSpecialCalendar();
+            MonthSpecialCalendar msc = new MonthSpecialCalendar();
 
             // test
             msc.setModel(i, 2014);
@@ -99,15 +78,7 @@ public class YearSpecialCalendar extends JPanel implements ActionListener
                 }
             }
             
-        }*/
-        add(mpMonth.get(0), new GridBagConstraints(0, 1, 1, 1, 0, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 5),
-                0, 0));
-        add(mpMonth.get(1), new GridBagConstraints(1, 1, 1, 1, 0, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 5),
-                0, 0));
-        add(mpMonth.get(2), new GridBagConstraints(2, 1, 1, 1, 0, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 5),
-                0, 0));
-        add(mpMonth.get(3), new GridBagConstraints(3, 1, 1, 1, 0, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 5),
-                0, 0));
+        }
 
     }
 
@@ -138,20 +109,11 @@ public class YearSpecialCalendar extends JPanel implements ActionListener
 
     public void setModel(int numYear)
     {
-        this.mpMonth.clear();
-        if (mpMonth.isEmpty())
+        for(int i = 0; i < 12; i++)
         {
-            msc.setModel(0, numYear);
-            mpMonth.put(0, msc);
-            
-            msc1.setModel(1, numYear);
-            mpMonth.put(1, msc1);
-            
-            msc2.setModel(2, numYear);
-            mpMonth.put(2, msc2);
-            
-            msc3.setModel(3, numYear);
-            mpMonth.put(3, msc3);
+            MonthSpecialCalendar msc = mpMonth.get(i);
+            msc.setModel(i, numYear);
+            mpMonth.put(i,  msc);
         }
     }
 
